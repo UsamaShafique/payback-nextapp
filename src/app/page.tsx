3f84178766e3b0956cdace74459c8294270244bc
Usman01234 <105125765+Usman01234@users.noreply.github.com>
2025-09-08 16:06:37 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 'use client'
 
+import Banner from "./components/Banner"
 import ConnectWallet from "./components/ConnectWallet"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <ConnectWallet />
+      {/* <ConnectWallet /> */}
+      <Header />
+      <Banner />
+      <Footer />
     </>
   )
 }
